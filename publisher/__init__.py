1a3900c3f1ce76baf3f07ee66fe2d408b8fa6870
Alex LordThorsen <alex.lordthorsen@esentire.com>
2019-06-16 23:15:42 -0700

    Added CLI and main structure; Stubs for tests.

---
+++ publisher/__init__.py
@@ -0,0 +1,18 @@
+import logging
+from pathlib import Path
+
+def load_version() -> str:
+    """Load the package version from the VERSION file so that all other python
+    files can use `import crs_normalizer; crs_normalizer.__version__`.
+    """
+    with open(Path(__file__).parent / "VERSION") as handle:
+        return handle.read().strip()
+
+__version__ = load_version()
+
+logging.basicConfig()
+logger = logging.getLogger(__name__) # pylint: disable=invalid-name
+
+from publisher.publisher import publish
+
+__all__ = ["__version__", "publish"]
--- tests/test_variable_substitution.py
+++ tests/test_variable_substitution.py
@@ -1,2 +1,18 @@
-def test_blank():
-    assert True
+
+def test_file_name_with_no_substitution():
+    assert False
+
+def test_file_name_with_single_substitution():
+    assert False
+
+def test_file_name_with_two_substitutions():
+    assert False
+
+def test_file_with_no_substitution():
+    assert False
+
+def test_file_with_single_substitution():
+    assert False
+
+def test_file_with_two_substitutions():
+    assert False
